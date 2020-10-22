8b2e9fc3f127c5f7d3d80f1967341d334e56a439
唐宋元明清 <dingjia_yu@126.com>
2020-10-22 16:31:28 +0800

    日历界面设计；

--- app/src/main/java/com/canace/yu_diary/module/calendar/CalendarFragment.java
+++ app/src/main/java/com/canace/yu_diary/module/calendar/CalendarFragment.java
@@ -1,15 +1,26 @@
 package com.canace.yu_diary.module.calendar;
 
+import android.os.Bundle;
 import android.view.View;
+
+import androidx.annotation.Nullable;
 
 import com.canace.yu_diary.R;
 import com.canace.yu_diary.app.BaseFragment;
+import com.necer.calendar.EmuiCalendar;
 
 public class CalendarFragment extends BaseFragment {
+
+    private EmuiCalendar emuiCalendar;
 
     @Override
     public View initView() {
         View view = View.inflate(context,R.layout.fragment_calendar,null);
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
     }
 }
